c39b352052762fec400e8280cafa28df26cee179
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2023-07-17 02:40:22 -0300

    Update thirdParty.ps1

--- thirdParty.ps1
+++ thirdParty.ps1
@@ -3,6 +3,8 @@ Invoke-WebRequest https://github.com/SnosMe/awakened-poe-trade/releases/download
 Invoke-WebRequest https://github.com/C1rdec/Poe-Lurker/releases/download/v1.21.15/PoeLurkerSetup.exe -OutFile $env:TEMP\PoeLurkerSetup.exe
 Invoke-WebRequest https://www.highrez.co.uk/scripts/download.asp?package=XMouse -OutFile $env:TEMP\XMouse.exe
 Invoke-WebRequest https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe -OutFile $env:TEMP\SteamSetup.exe
+Invoke-WebRequest https://github.com/PathOfBuildingCommunity/PathOfBuilding/releases/download/v2.30.1/PathOfBuildingCommunity-Setup-2.30.1.exe -OutFile $env:TEMP/PathOfBuildingCommunity-Setup-2.30.1.exe
+. $env:TEMP\PathOfBuildingCommunity-Setup-2.30.1.exe
 . $env:TEMP\SteamSetup.exe
 . $env:TEMP\Awakened-PoE-Trade-Setup.exe
 . $env:TEMP\PoeLurkerSetup.exe
