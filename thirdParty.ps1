03c451fea3288a5f70cda0b6577df00765baef8f
Mikael Amaral <55303570+Mikaral@users.noreply.github.com>
2023-08-09 13:36:34 -0300

    modified:   thirdParty.ps1

--- thirdParty.ps1
+++ thirdParty.ps1
@@ -4,9 +4,9 @@ Invoke-WebRequest https://github.com/C1rdec/Poe-Lurker/releases/download/v1.21.1
 Invoke-WebRequest https://www.highrez.co.uk/scripts/download.asp?package=XMouse -OutFile $env:TEMP\XMouse.exe
 Invoke-WebRequest https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe -OutFile $env:TEMP\SteamSetup.exe
 Invoke-WebRequest https://github.com/PathOfBuildingCommunity/PathOfBuilding/releases/download/v2.30.1/PathOfBuildingCommunity-Setup-2.30.1.exe -OutFile $env:TEMP/PathOfBuildingCommunity-Setup-2.30.1.exe
-. $env:TEMP\PathOfBuildingCommunity-Setup-2.30.1.exe
-. $env:TEMP\SteamSetup.exe
-. $env:TEMP\Awakened-PoE-Trade-Setup.exe
-. $env:TEMP\PoeLurkerSetup.exe
-. $env:TEMP\GeForce_Experience_v3.27.0.112.exe
-. $env:TEMP\XMouse.exe
+Start-Process -FilePath $env:TEMP\PathOfBuildingCommunity-Setup-2.30.1.exe
+Start-Process -FilePath $env:TEMP\SteamSetup.exe
+Start-Process -FilePath $env:TEMP\Awakened-PoE-Trade-Setup.exe
+Start-Process -FilePath $env:TEMP\PoeLurkerSetup.exe
+Start-Process -FilePath $env:TEMP\GeForce_Experience_v3.27.0.112.exe
+Start-Process -FilePath $env:TEMP\XMouse.exe
